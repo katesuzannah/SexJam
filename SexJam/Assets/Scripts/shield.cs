569afa042f6fd5c2216ce9f6540126cb18c38dcc
Kate Smith <katesuzannah@gmail.com>
2017-11-11 16:55:11 -0500

    Sperm rotation is correct now, added sprites to circle and blocker

--- SexJam/Assets/Scripts/shield.cs
+++ SexJam/Assets/Scripts/shield.cs
@@ -22,11 +22,11 @@ public class shield : MonoBehaviour {
 //		}
 //		transform.position = (Vector2) center.position + Geo.ToVect(ang) * radius;
 	}
-	void OnCollisionEnter2D (Collision2D col) {
-		if (col.gameObject.tag == "sperm") {
-			InstantiationManager.totalThings--;
-			InstantiationManager.deflected++;
-			Destroy (col.gameObject);
-		}
-	}
+//	void OnCollisionEnter2D (Collision2D col) {
+//		if (col.gameObject.tag == "sperm") {
+//			InstantiationManager.totalThings--;
+//			InstantiationManager.deflected++;
+//			Destroy (col.gameObject);
+//		}
+//	}
 }
