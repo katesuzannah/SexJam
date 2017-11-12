fad4d0f1d1d7c8541127268eadd56a3e28abc1bf
jimistine <jrstine@me.com>
2017-11-11 19:00:03 -0500

    Added fungus and a quick mockup

---
+++ SexJam/Assets/Scripts/FungusTestActivate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FungusTestActivate : MonoBehaviour
+{
+	public GameObject flowchart;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			flowchart.SetActive(true);
+		}
+	}
+}
