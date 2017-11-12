5f62ec59e6a52e12a8ecf21b3d261d8f5ebb75c9
jimistine <jrstine@me.com>
2017-11-11 20:27:29 -0500

    Added start scene
    and the scene swapper

---
+++ SexJam/Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class StartGameButton : MonoBehaviour {
+
+	public Button myButton;
+	
+	void Start()
+	{
+	
+//		Button btn = myButton.GetComponent<Button>();
+		
+	}
+
+//	void Update()
+//	{
+//		bool btn;
+//		if (btn == true)
+//		{
+//			StartGame();
+//		}
+//	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("2DMain");
+	}
+}
